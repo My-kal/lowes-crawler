7e07e98e527d5bc93d17901708940f7714f98af7
Mykal <devmykal@protonmail.com>
2024-11-19 11:01:31 -0500

    Use JSON config to store start urls

--- lowes_crawler/lowes_crawler/spiders/lowes.py
+++ lowes_crawler/lowes_crawler/spiders/lowes.py
@@ -1,10 +1,21 @@
 import scrapy
-
+import json
 
 class LowesSpider(scrapy.Spider):
     name = "lowes"
     allowed_domains = ["lowes.com"]
-    start_urls = ["https://lowes.com"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Load the config file
+        with open("config.json", "r") as file:
+            config = json.load(file)
+
+        # Set start_urls from config
+        self.start_urls = config.get("start_urls", [])
+
+        # Log loaded start_urls
+        self.logger.info(f"Loaded start_urls: {self.start_urls}")
 
     def parse(self, response):
         pass

997ce9b133eebafe2addcbde32b4171b2b380424
Mykal <devmykal@protonmail.com>
2024-11-20 20:03:58 -0500

    Added rotate useragents setting and turned off url duplicate filter

--- lowes_crawler/lowes_crawler/settings.py
+++ lowes_crawler/lowes_crawler/settings.py
@@ -17,17 +17,23 @@ NEWSPIDER_MODULE = "lowes_crawler.spiders"
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:132.0) Gecko/20100101 Firefox/132.0"
+FAKEUSERAGENT_PROVIDERS = [
+	'scrapy_fake_useragent.providers.FakeUserAgentProvider',  # this is the first provider we'll try
+	'scrapy_fake_useragent.providers.FakerProvider',  # if FakeUserAgentProvider fails, we'll use faker to generate a user-agent string for us
+	'scrapy_fake_useragent.providers.FixedUserAgentProvider',  # fall back to USER_AGENT value
+]
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 15
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 1
+RANDOMIZE_DOWNLOAD_DELAY = True
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -111,3 +117,5 @@ FEEDS = {
 RETRY_ENABLED = True
 RETRY_TIMES = 25  # Number of retries
 RETRY_HTTP_CODES = [403, 500, 502, 503, 504]
+
+DUPEFILTER_CLASS = 'scrapy.dupefilters.BaseDupeFilter'
